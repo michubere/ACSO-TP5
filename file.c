7624dbae7b17a3c8137af2bbb1557a8bcee067f1
Michelle <michelle@Michelles-MacBook-Pro.local>
2024-06-08 15:57:16 -0300

    nivel inodo

--- file.c
+++ file.c
@@ -10,6 +10,33 @@
  */
 int file_getblock(struct unixfilesystem *fs, int inumber, int blockNum, void *buf) {
     
+    struct inode inp;
+    int inode = inode_iget(fs, inumber, &inp);
+    if (inode == -1) {
+        return -1;
+    }
+
+    int disk_block = inode_indexlookup(fs, &inp, blockNum);
+    if (disk_block == -1) {
+        return -1;
+    }
+
+    int read = diskimg_readsector(fs->dfd, disk_block, buf);
+    if (read == -1) {
+        return -1;
+    }
+
+    int bytes = inode_getsize(&inp);
+    if (bytes < 0) {
+        return -1;
+    }
+    
+    int blocks = bytes / DISKIMG_SECTOR_SIZE;
+    if (blockNum == blocks) {
+        return bytes % DISKIMG_SECTOR_SIZE;
+    } else {
+        return DISKIMG_SECTOR_SIZE;
+    }
     return 0;
 }
 
--- inode.c
+++ inode.c
@@ -4,12 +4,28 @@
 #include "inode.h"
 #include "diskimg.h"
 
+#define INDIR_ADDR = 7;
 
 /**
  * TODO
  */
 int inode_iget(struct unixfilesystem *fs, int inumber, struct inode *inp) {
-    //Implement Code Here
+    if (inumber < 1 || inumber > fs->superblock.s_ninodes) {
+        return -1;
+    }
+
+    int inodes_per_block = DISKIMG_SECTOR_SIZE / sizeof(struct inode);
+    int block_num = (inumber - 1) / inodes_per_block; //ver si agregar +2 o no
+    int offset = (inumber - 1) % inodes_per_block;
+
+    struct inode inodes[inodes_per_block];
+    int read = diskimg_readsector(fs->dfd, block_num, inp);
+    if (read == -1) {
+        return -1;
+    }
+
+    *inp = inodes[offset];
+
     return 0; 
 }
 
@@ -18,7 +34,47 @@ int inode_iget(struct unixfilesystem *fs, int inumber, struct inode *inp) {
  */
 int inode_indexlookup(struct unixfilesystem *fs, struct inode *inp,
     int blockNum) {  
-        //Implement code here
+    if (blockNum < 0 || blockNum >= 8) {
+        return -1;
+    }
+
+    // small file
+    if ((inp->i_mode & ILARG) == 0) {
+        return inp->i_addr[blockNum];
+    }
+
+    // large file
+    int address_number = DISKIMG_SECTOR_SIZE / sizeof(uint16_t);
+    int indirect_address_number = address_number * INDIR_ADDR;
+    if (blockNum < indirect_address_number) {
+        int offset = blockNum / address_number;
+        uint16_t address[address_number];
+        int read = diskimg_readsector(fs->dfd, inp->i_addr[offset], address);
+        if (read == -1) {
+            return -1;
+        }
+
+        return address[blockNum % address_number];
+    } else {
+        // layer one
+        int blockNum_double = blockNum - indirect_address_number;
+        int offset1 = blockNum_double / address_number;
+        uint16_t address1[address_number];
+        int read1 = diskimg_readsector(fs->dfd, inp->i_addr[offset1], address1);
+        if (read1 == -1) {
+            return -1;
+        }
+
+        // layer two
+        int offset2 = blockNum_double % address_number;
+        uint16_t address2[address_number];
+        int read2 = diskimg_readsector(fs->dfd, address1[offset1], address2);
+        if (read2 == -1) {
+            return -1;
+        }
+        return address2[offset2];
+    }
+
     return 0;
 }
 
